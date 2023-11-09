90e678eaba53e784911f103dc2dd3622575e6b1d
gprohorov <g.prokhorov@indevlab.com>
2023-11-09 19:28:24 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+/*
+  @author   george
+  @project   Default (Template) Project
+  @class  ${NAME}
+  @version  1.0.0 
+  @since 09.11.23 - 19.26
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
